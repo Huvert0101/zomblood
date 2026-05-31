486a254a26cfda85b39cc0c6ac9b0d3f616b7a8a
Huvert01 <lupi2102casrillo@gmail.com>
2026-05-31 09:50:36 -0600

    adding pixelated graphics

--- main.js
+++ main.js
@@ -14,7 +14,10 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth/2, window.innerHeight/2);
+renderer.domElement.style.width = "100vw";
+renderer.domElement.style.height = "100vh";
+renderer.domElement.style.imageRendering = "pixelated";
 document.body.appendChild( renderer.domElement );
 
 //LUZ AMBIENTAL
